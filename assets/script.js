7f1c30817a1df490be8bb6c242e53df73859b77a
Knock <enoch10evelyn@gmail.com>
2022-08-30 21:48:20 -0700

    API call function for city lon and lat is working

--- assets/script.js
+++ assets/script.js
@@ -3,16 +3,18 @@ var requestUrl = "https://api.openweathermap.org/data/2.5/weather?lat=40.6576&lo
 // var APIKey = "d730384eadcace798781efeee25eace8";
 var cityInput = document.querySelector(".city-input");
 var button = document.querySelector(".btn");
-debugger //debugs everthing before it and can be tested in console.
+//debugger //debugs everthing before it and can be tested in console.
 button.addEventListener("click", function(){
-    var requestCity = "http://api.openweathermap.org/geo/1.0/direct?q=Freeport&appid=d730384eadcace798781efeee25eace8" 
+    var cityInput = document.querySelector(".city-input");
+    var requestCity = "http://api.openweathermap.org/geo/1.0/direct?q=" +cityInput.value+ "&appid=d730384eadcace798781efeee25eace8" 
+
     fetch(requestCity).then(function(response){
         response.json().then(function(data){
             console.log(data);
         }); 
 });
 
-}
+})
 
 
 //    fetch(requestUrl).then(function(response){
@@ -20,10 +22,10 @@ button.addEventListener("click", function(){
 //             console.log(data);
 // //         }); 
 // });
-   fetch(requestCity).then(function(response){
-        response.json().then(function(data){
-            console.log(data);
-        }); 
-});
+//    fetch(requestCity).then(function(response){
+//         response.json().then(function(data){
+//             console.log(data);
+//         }); 
+// });
 
 
