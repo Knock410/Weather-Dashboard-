8d6f5591a9b3e988a71e0af9e982c369f46e7cfb
Knock <enoch10evelyn@gmail.com>
2022-08-30 21:24:50 -0700

    altered api call and added city parameter

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,16 @@
-var requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
+var requestUrl = "https://api.openweathermap.org/data/2.5/weather?lat=40.6576&lon=-73.5832&appid=d730384eadcace798781efeee25eace8"
+var requestCity = "http://api.openweathermap.org/geo/1.0/direct?q=Freeport&appid=d730384eadcace798781efeee25eace8"
 var APIKey = "d730384eadcace798781efeee25eace8";
 
-fetch(requestUrl)
-.then(function(response){
-    return response.json();
-})
-.then(function(date){
-    console.log(date);
+
+//    fetch(requestUrl).then(function(response){
+//         response.json().then(function(data){
+//             console.log(data);
+// //         }); 
+// });
+   fetch(requestCity).then(function(response){
+        response.json().then(function(data){
+            console.log(data);
+        }); 
 });
+
